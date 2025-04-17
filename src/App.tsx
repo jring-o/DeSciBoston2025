9fcfee752766050aa3df01dc73399ebd7b78b029
J S <jringo303@gmail.com>
2025-04-17 14:32:20 -0400

    feat: Update site content and configure for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// App.tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +18,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Pass the basename prop here */}
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
